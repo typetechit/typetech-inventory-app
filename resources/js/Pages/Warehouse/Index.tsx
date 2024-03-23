c5c81cad2dec62df3b982b691010faf130c89b40
Saifur Rahman <saifur.dohs@gmail.com>
2024-03-23 16:29:47 +0600

    Warehouse Page Action component added

--- resources/js/Pages/Warehouse/Index.tsx
+++ resources/js/Pages/Warehouse/Index.tsx
@@ -1,11 +1,18 @@
 import AuthenticatedLayout, {DashboardPageContainer, PageHeader} from '@/Layouts/AuthenticatedLayout';
-import { Head } from '@inertiajs/react';
+import {Head, Link} from '@inertiajs/react';
 import { PageProps } from '@/types';
 import {Card, CardContent, CardDescription, CardHeader, CardTitle} from "@/Components/ui/card";
-import MultiSelectInput from "@/Components/ui/MultiSelectInput";
-import {useState} from "react";
-import {cn} from "@/lib/utils";
-import {Loading} from "@/Components/Loading";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/Components/ui/dropdown-menu"
+import {Button} from "@/Components/ui/button";
+import {ChevronDown} from "lucide-react";
+
 
 export default function WarehouseIndex({auth}: PageProps) {
     return (
@@ -28,10 +35,96 @@ export default function WarehouseIndex({auth}: PageProps) {
 }
 
 
+function StocksDropdown() {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Button type={'button'} variant={'outline'} className={`flex items-center gap-2`}>
+                    Stocks
+                    <ChevronDown className={`w-4 h-4 text-gray-500`} />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem>
+                    <Link href={'/warehouse/stocks'}>Current Stocks</Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem>
+                    <Link href={'/warehouse/stocks/stocks-in'}>Stock In</Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem>
+                    <Link href={'/warehouse/stocks/stocks-out'}>Stock In</Link>
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
+
+function SupplierOrdersDropdown() {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Button type={'button'} variant={'outline'} className={`flex items-center gap-2`}>
+                    Supplier Orders
+                    <ChevronDown className={`w-4 h-4 text-gray-500`} />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem>
+                    <Link href={'/suppliers/orders'}>All Orders</Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem>
+                    <Link href={'/suppliers/orders/find'}>Find Order</Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem>
+                    <Link href={'/suppliers/orders/create'}>Create Order</Link>
+                </DropdownMenuItem>
+
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
+
+function BranchOrdersDropdown() {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Button type={'button'} variant={'outline'} className={`flex items-center gap-2`}>
+                    Branch Orders
+                    <ChevronDown className={`w-4 h-4 text-gray-500`} />
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+                <DropdownMenuItem>
+                    <Link href={'/branches/orders'}>All Orders</Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem>
+                    <Link href={'/branches/orders/find'}>Find Order</Link>
+                </DropdownMenuItem>
+
+                <DropdownMenuItem>
+                    <Link href={'/branches/orders/create'}>Create Order</Link>
+                </DropdownMenuItem>
+
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+}
+
 export function WarehousePageAction () {
     return (
-        <div>
-            Warehouse Page Action
+        <div className={`flex items-center gap-4`}>
+
+            <StocksDropdown />
+
+            <SupplierOrdersDropdown />
+
+            <BranchOrdersDropdown />
+
         </div>
     );
 }
